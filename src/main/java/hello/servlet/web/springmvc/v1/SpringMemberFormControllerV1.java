b42f93b1362facc4bfa2e7221d12ac03e5e18444
norma <norma@normas-MacBook-Pro.local>
2024-02-23 13:39:26 +0900

    first init

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.v1;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        System.out.println("SpringMemberFormControllerV1.handlerRequest");
+        return new ModelAndView("new-form");
+    }
+}
